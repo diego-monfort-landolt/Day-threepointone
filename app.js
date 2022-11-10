b13a996f5e1fd89583d2949c5f4418187d8ef2fc
Diego19890102 <diegomonfort1989@gmail.com>
2022-11-10 16:42:55 +0100

    new
    text animation whit js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 1000,
+    delay: (el, i) => 70 * (i+1)
+ });
+//Wrap every letter in a span
